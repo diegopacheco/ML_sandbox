bf556f3b04582fd74beafc618f7b92c53bc17094
diegopacheco <diego.pacheco.it@gmail.com>
2015-11-19 22:27:48 -0800

    added Text Regression

--- Scala/scala_smile_ML_playground/build.sbt
+++ Scala/scala_smile_ML_playground/build.sbt
@@ -6,13 +6,16 @@ scalaVersion := "2.11.7"
 
 scalaVersion in ThisBuild := "2.11.7"
 
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "4.0.0")
+
 resolvers += "Akka Repo" at "http://repo.akka.io/releases" 
 resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
 resolvers += "Sonatype Repo" at "https://oss.sonatype.org/content/repositories/releases/"
 resolvers += "Sonatype snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"
 resolvers += "Twitter Repo" at "http://maven.twttr.com/"
+resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
 
 libraryDependencies += "com.github.haifengl" % "smile-core" % "1.0.3"
 libraryDependencies += "com.github.haifengl" % "smile-plot" % "1.0.3"
 libraryDependencies += "org.scala-lang.modules" % "scala-swing_2.11" % "2.0.0-M2"
-
+libraryDependencies += "com.github.tototoshi" % "scala-csv" % "1.2.2"
---
+++ Scala/scala_smile_ML_playground/src/main/scala/com/github/diegopacheco/sandbox/ml/scala/smile/textregression/TextRegression.scala
@@ -0,0 +1,62 @@
+package com.github.diegopacheco.sandbox.ml.scala.smile.textregression
+
+import java.awt.Color
+import java.io.File
+import java.util.Calendar
+import com.github.tototoshi.csv._
+import smile.plot._
+import smile.regression.{LASSO, RidgeRegression}
+import smile.validation.CrossValidation
+import scala.swing._
+
+
+object TextRegression extends App {
+  
+   //Get the example data
+    val basePath = "/users/mikedewaard/MachineLearning/Example Data/TextRegression_Example_4.csv"
+    val testData = GetDataFromCSV(new File(basePath))
+
+    //Create a document term matrix for the data
+    val documentTermMatrix = new DTM()
+    testData.foreach(x => documentTermMatrix.addDocumentToRecords(x._1, x._2, x._3))
+
+    //Get the cross validation data
+    val cv = new CrossValidation(testData.length, 2)
+    val numericDTM = documentTermMatrix.getNumericRepresentationForRecords
+
+    for (i <- 0 until cv.k) {
+      //Split off the training datapoints and classifiers from the dataset
+      val dpForTraining = numericDTM._1.zipWithIndex.filter(x => cv.test(i).toList.contains(x._2)).map(y => y._1)
+      val classifiersForTraining = numericDTM._2.zipWithIndex.filter(x => cv.test(i).toList.contains(x._2)).map(y => y._1)
+
+      //And the corresponding subset of data points and their classifiers for testing
+      val dpForTesting = numericDTM._1.zipWithIndex.filter(x => !cv.test(i).contains(x._2)).map(y => y._1)
+      val classifiersForTesting = numericDTM._2.zipWithIndex.filter(x => !cv.test(i).contains(x._2)).map(y => y._1)
+
+      //These are the lambda values we will verify against
+      val lambdas: Array[Double] = Array(0.1, 0.25, 0.5, 10.0, 20.0, 50.0)
+
+      lambdas.foreach { x =>
+        //Define a new model based on the training data and one of the lambda's
+        val model = new LASSO(dpForTraining, classifiersForTraining, x)
+
+        //Compute the RMSE for this model with this lambda
+        val results = dpForTesting.map(y => model.predict(y)) zip classifiersForTesting
+        val RMSE = Math.sqrt(results.map(x => Math.pow(x._1 - x._2, 2)).sum / results.length)
+        println(Calendar.getInstance().getTime + "Lambda: " + x + " RMSE: " + RMSE)
+
+      }
+    }
+ 
+  def GetDataFromCSV(file: File) : List[(String,Int,String)]= {
+    val reader = CSVReader.open(file)
+    val data = reader.all()
+
+    val documents = data.drop(1).map(x => (x(1),x(3).toInt,x(4)))
+    documents
+  }
+
+
+
+  
+}
